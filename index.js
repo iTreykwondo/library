1a16684fb13f73e733e3cce15684380d42fb7be3
Trajan Miller <trajanmiller324@gmail.com>
2022-12-27 10:48:01 -0500

    Display all books in a card from the mylibrary array

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const cardContainer = document.querySelector(".main");
+
+let myLibrary = [];
+
 function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
@@ -11,5 +15,34 @@ function Book(title, author, pages, readStatus) {
   };
 }
 
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  return;
+}
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(theHobbit.info());
+const testBook = new Book("Test", "Mr. Test", 300, false);
+addBookToLibrary(theHobbit);
+addBookToLibrary(testBook);
+console.log(myLibrary);
+
+myLibrary.forEach((book) => {
+  let bookCard = document.createElement("div");
+  let title = document.createElement("h2");
+  let author = document.createElement("h3");
+  let pages = document.createElement("p");
+  let readStatus = document.createElement("p");
+
+  title.textContent = book.title;
+  author.textContent = `by ${book.author}`;
+  pages.textContent = `${book.pages} pages`;
+  readStatus.textContent = book.readStatus ? "Read" : "Not read yet";
+
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(readStatus);
+  bookCard.classList.add("card");
+
+  cardContainer.appendChild(bookCard);
+});
